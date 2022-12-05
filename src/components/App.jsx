bdd297d4591a03ad92492961ee44597708a1293d
Sergii Dychko <110810294+SergiiDychko@users.noreply.github.com>
2022-12-05 16:08:34 +0200

    додав слеш перед *

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,7 +19,7 @@ export default function App() {
           <Route path="cast" element={<Cast />} />
           <Route path="reviews" element={<Reviews />} />
         </Route>
-        <Route path="*" element={<Home />} />
+        <Route path="/*" element={<Home />} />
       </Route>
     </Routes>
   );
