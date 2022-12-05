8fa8704b00c3de5f7aa22de994cba96c41bd817e
Sergii Dychko <110810294+SergiiDychko@users.noreply.github.com>
2022-12-05 16:16:14 +0200

    відкотив назад на BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {HashRouter} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 import App from './components/App';
 import './index.css';
  
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter basename="/goit-react-hw-05-movies">
+    <BrowserRouter basename="/goit-react-hw-05-movies">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
