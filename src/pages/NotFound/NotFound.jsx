5ce817bb132d8106ab39223f789f400001c89840
Sergii Dychko <110810294+SergiiDychko@users.noreply.github.com>
2022-12-05 15:26:22 +0200

    комміт

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -4,7 +4,7 @@ import { StyledNotify } from "./NotFound.styled";
 export default function pageNotFound() {
     return (
       <StyledNotify>
-            <p>Тo promises, no apologies.</p>
+            <p>No promises, no apologies.</p>
             <p>404 happened by itself.</p>
       </StyledNotify>
     );
